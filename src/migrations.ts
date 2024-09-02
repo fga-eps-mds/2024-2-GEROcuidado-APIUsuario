e2498a99c10253d985b08d80bc83ce743bf7b68f
Victor Gonçalves <victor.eng.unb@gmail.com>
2024-09-01 23:37:58 -0300

    feat: add created at and updated at fields

--- src/migrations.ts
+++ src/migrations.ts
@@ -14,6 +14,7 @@ import { DataSource } from 'typeorm';
     migrationsRun: true,
     logging: true,
     migrations: [__dirname + '/migration/*.js'],
+    // namingStrategy: new SnakeNamingStrategy(), // Here you'r using the strategy!,
   });
 
   await dataSource.initialize();
